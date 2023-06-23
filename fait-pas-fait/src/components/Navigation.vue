a8d4f4da046d33b1b27778bd8cba330bfad14420
charlesrb <charles.reverchon.billot@gmail.com>
2023-06-23 22:56:44 +0200

    Fix : routes

--- fait-pas-fait/src/components/Navigation.vue
+++ fait-pas-fait/src/components/Navigation.vue
@@ -1,5 +1,6 @@
 <template>
     <nav>
+      <router-link to="/">Home</router-link><br/>
       <router-link to="/fait-pas-fait">Fait pas fait</router-link><br/>
       <router-link to="/timeline">Timeline</router-link>
     </nav>
--- fait-pas-fait/src/router/index.js
+++ fait-pas-fait/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import FaitPasFait from "../views/fait-pas-fait.vue";
 import Timeline from "../views/timeline.vue";
+import Home from "../views/home.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home,
+          },
       {
         path: "/fait-pas-fait",
         name: "fait-pas-fait",
