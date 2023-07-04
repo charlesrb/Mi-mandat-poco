ff30310171576f4698ba13e735ad12205a5da2f6
charlesrb <charles.reverchon.billot@gmail.com>
2023-07-04 13:50:56 +0200

    test embed

--- fait-pas-fait/src/views/home.vue
+++ fait-pas-fait/src/views/home.vue
@@ -3,15 +3,23 @@
     <Navigation></Navigation>
     <h1>Bilan mi-mandat Poitiers Collectif</h1>
     <h2>Il faut voir ce qu'on veut raconter sur cette home&#xA0;!</h2>
-
+    <vue-pdf-embed :source="mySource" />
   </main>
 </template>
 
 <script>
 import Navigation from "../components/Navigation.vue";
+import VuePdfEmbed from 'vue-pdf-embed'
+
+
 export default {
-  components: { Navigation },
-};
+  components: { Navigation, VuePdfEmbed },
+  data() {
+    return {
+      mySource: 'https://poitierscollectif.fr/wp-content/uploads/2023/07/bilan-poitiers-collectif.pdf'
+    }
+  }
+}
 </script>
 
 <style scoped>
--- fait-pas-fait/src/views/timeline.vue
+++ fait-pas-fait/src/views/timeline.vue
@@ -2,30 +2,54 @@
   <main>
     <Navigation></Navigation>
     <h1>Timeline 2020 - 2023</h1>
-    <h2>Voilà maintenant trois ans que nous sommes élu.e.s... Trois ans de gestion d'une ville au quotidien, de belles réalisations, de belles projections pour les années à venir, mais aussi trois ans d'apprentissage de la vie d'élu.e.s ! Pour nous qui portons l'ambition de permettre à chaque citoyen.ne de s'engager dans la vie politique locale, endosser le costume, concilier vie professionnelle, familiale et politique, ne fut pas toujours chose facile. Sans parler des multiples surprises que nous a réservées ce début de mandat ! Retrouvez ici un aperçu de nos trois années vécues, à hauteur d'hommes et de femmes élu.e.s la fleur au fusil, et engagé.e.s comme au premier jour</h2>
+    <h2>
+      Voilà maintenant trois ans que nous sommes élu.e.s... Trois ans de gestion
+      d'une ville au quotidien, de belles réalisations, de belles projections
+      pour les années à venir, mais aussi trois ans d'apprentissage de la vie
+      d'élu.e.s ! Pour nous qui portons l'ambition de permettre à chaque
+      citoyen.ne de s'engager dans la vie politique locale, endosser le costume,
+      concilier vie professionnelle, familiale et politique, ne fut pas toujours
+      chose facile. Sans parler des multiples surprises que nous a réservées ce
+      début de mandat ! Retrouvez ici un aperçu de nos trois années vécues, à
+      hauteur d'hommes et de femmes élu.es, et engagé.e.s comme
+      au premier jour
+    </h2>
 
     <div class="container">
       <div class="timeline">
-        <div class="timeline-container primary" v-for="action in timeline" :key="action.id">
+        <div
+          class="timeline-container primary"
+          v-for="action in timeline"
+          :key="action.id"
+        >
           <div class="timeline-icon">
-            <img :src="action.imgelu" alt="">
+            <img :src="action.imgelu" alt="" />
             <i class="far fa-grin-wink"></i>
           </div>
           <div class="timeline-body">
             <!-- <div class="timeline-elu">
             <img :src="action.imgelu" alt=""> -->
-            <p class="date">{{ action.date }} - <span class="elu">{{ action.elu }}</span></p>
-          <!-- </div> -->
-            <img :src="action.img" class="timeline-img" alt="" v-if="action.img">
-            <h4 class="timeline-title"><span class="badge">{{ action.qualification }}</span></h4>
+            <p class="date">
+              {{ action.date }} - <span class="elu">{{ action.elu }}</span>
+            </p>
+            <!-- </div> -->
+            <img
+              :src="action.img"
+              class="timeline-img"
+              alt=""
+              v-if="action.img"
+            />
+            <h4 class="timeline-title">
+              <span class="badge">{{ action.qualification }}</span>
+            </h4>
             <h5>{{ action.titre }}</h5>
             <p>
-              {{action.descriptif}}
+              {{ action.descriptif }}
             </p>
             <!-- <p class="timeline-subtitle">{{action.elu}}</p> -->
           </div>
         </div>
-<!-- 
+        <!-- 
 
         <div class="timeline-container danger">
           <div class="timeline-icon">
@@ -117,10 +141,9 @@ export default {
   components: { Navigation },
   data() {
     return {
-      timeline: Timeline
-     
+      timeline: Timeline,
     };
-}
+  },
 };
 </script>
 
