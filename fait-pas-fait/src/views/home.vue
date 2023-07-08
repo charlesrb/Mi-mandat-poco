9b41556cc5af4f12677d2c73f119f8efc2cdf62c
charlesrb <charles.reverchon.billot@gmail.com>
2023-07-08 12:48:05 +0200

    fix link home

--- fait-pas-fait/src/views/home.vue
+++ fait-pas-fait/src/views/home.vue
@@ -3,9 +3,7 @@
     <Navigation></Navigation>
     <h1>Bilan mi-mandat Poitiers Collectif</h1>
     <h2>Bienvenue sur le site dédié au mi-mandat de Poitiers Collectif ! <br/>
-      Vous y retrouverez le <a href="https://poitierscollectif.fr/wp-content/uploads/2023/07/bilan-poitiers-collectif.pdf">bilan détaillé</a>, le <a href="/bilan-de-nos-actions">bilan de nos actions</a> et la <a href="/timeline">timeline</a> des trois dernières années ! Bonne visite !</h2>
-    <!-- <vue-pdf-embed :source="mySource" /> -->
-    <!-- <iframe src="https://poitierscollectif.fr/wp-content/uploads/2023/07/bilan-poitiers-collectif.pdf" width="100%" height="700px"> </iframe> -->
+      Vous y retrouverez le <a href="https://poitierscollectif.fr/wp-content/uploads/2023/07/bilan-poitiers-collectif.pdf">bilan détaillé</a>, le <a href="/bilan-de-nos-actions.html">bilan de nos actions</a> et la <a href="/timeline.html">timeline</a> des trois dernières années ! Bonne visite !</h2>
     <iframe src="//v.calameo.com/?bkcode=00741617240c58f9db1f5&mode=mini" width="100%" height="500" frameborder="0" scrolling="no" allowtransparency allowfullscreen></iframe>
 </main>
 </template>
